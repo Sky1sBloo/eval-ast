f5e77c0d231a03ef6e073cae10da2d39bc568278
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-09 17:04:24 +0800

    Formatted Parse tree header

--- src/ParseTree.hpp
+++ src/ParseTree.hpp
@@ -9,61 +9,54 @@
 
 class StatementNode;
 
-template <typename T>
-class ExpressionNode;
+template <typename T> class ExpressionNode;
+
+template <typename T> class ConstantNode;
+
+template <typename T> class BinaryOperationNode;
+
+template <typename T> class PrintNode;
 
 template <typename T>
-class ConstantNode;
-
-template <typename T>
-class BinaryOperationNode;
-
-template <typename T>
-class PrintNode;
-
-template <typename T>
-using ExpressionNodeContainer = std::variant<
-    std::unique_ptr<ExpressionNode<T>>,
-    std::unique_ptr<ConstantNode<T>>,
-    std::unique_ptr<BinaryOperationNode<T>>>;
+using ExpressionNodeContainer = std::variant<std::unique_ptr<ExpressionNode<T>>, std::unique_ptr<ConstantNode<T>>,
+                                             std::unique_ptr<BinaryOperationNode<T>>>;
 
 class StatementNode
 {
-public:
-	virtual ~StatementNode() {};
-	virtual void doStatement() = 0;
+  public:
+    virtual ~StatementNode() {};
+    virtual void doStatement() = 0;
 };
 
-
-template <typename T>
-class ExpressionNode 
+template <typename T> class ExpressionNode
 {
-public:
-	virtual T getValue() const = 0;
+  public:
+    virtual T getValue() const = 0;
 };
 
-
-template <typename T>
-class ConstantNode : public ExpressionNode<T>
+template <typename T> class ConstantNode : public ExpressionNode<T>
 {
-public:
-	ConstantNode(T value) : mValue(value) {}
-	T getValue() const override { return mValue; }
-private:
-	T mValue;
+  public:
+    ConstantNode(T value) : mValue(value)
+    {
+    }
+    T getValue() const override
+    {
+        return mValue;
+    }
+
+  private:
+    T mValue;
 };
 
-
-template <typename T>
-class BinaryOperationNode : public ExpressionNode<T>
+template <typename T> class BinaryOperationNode : public ExpressionNode<T>
 {
-public:
-	BinaryOperationNode(BinaryOperators operation) :
-		mValueA(std::unique_ptr<ExpressionNode<T>>(nullptr)),
-		mValueB(std::unique_ptr<ExpressionNode<T>>(nullptr)),
-		mOperation(operation)
-	{
-	}
+  public:
+    BinaryOperationNode(BinaryOperators operation)
+        : mValueA(std::unique_ptr<ExpressionNode<T>>(nullptr)), mValueB(std::unique_ptr<ExpressionNode<T>>(nullptr)),
+          mOperation(operation)
+    {
+    }
 
     /**
      * Constructor for appending binary operation
@@ -71,58 +64,62 @@ public:
      * @operation that will be used
      * @value ValueB that will be appended
      */
-    BinaryOperationNode(BinaryOperators operation, std::unique_ptr<ExpressionNode<T>> value) :
-		mValueA(std::unique_ptr<ExpressionNode<T>>(nullptr)),
-        mValueB(std::move(value)),
-        mOperation(operation)
+    BinaryOperationNode(BinaryOperators operation, std::unique_ptr<ExpressionNode<T>> value)
+        : mValueA(std::unique_ptr<ExpressionNode<T>>(nullptr)), mValueB(std::move(value)), mOperation(operation)
     {
     }
 
-	BinaryOperationNode(BinaryOperators operation, std::unique_ptr<ExpressionNode<T>> valueA, std::unique_ptr<ExpressionNode<T>> valueB) :
-		mValueA(std::move(valueA)),
-		mValueB(std::move(valueB)),
-		mOperation(operation)
-	{
-	}
+    BinaryOperationNode(BinaryOperators operation, std::unique_ptr<ExpressionNode<T>> valueA,
+                        std::unique_ptr<ExpressionNode<T>> valueB)
+        : mValueA(std::move(valueA)), mValueB(std::move(valueB)), mOperation(operation)
+    {
+    }
 
-	T getValue() const override
-	{
-        T value = std::visit([this](const auto& valueA, const auto& valueB) {
-            if (valueA == nullptr || valueB == nullptr) {
-			    throw std::runtime_error("Invalid binary operation: One of the values is nullptr");
-            }
+    T getValue() const override
+    {
+        T value = std::visit(
+            [this](const auto &valueA, const auto &valueB) {
+                if (valueA == nullptr || valueB == nullptr)
+                {
+                    throw std::runtime_error("Invalid binary operation: One of the values is nullptr");
+                }
 
-            switch (mOperation)
-	    	{
-	    		case BinaryOperators::ADDITION:
-	    			return valueA->getValue() + valueB->getValue();
-	    		case BinaryOperators::SUBTRACTION:
-	    			return valueA->getValue() - valueB->getValue();
-	    		case BinaryOperators::MULTIPLICATION:
-	    			return valueA->getValue() * valueB->getValue();
-	    		case BinaryOperators::DIVISION:
-	    			return valueA->getValue() / valueB->getValue();
-	    	}
-	    	throw std::runtime_error("Invalid binary operation: Invalid operation");  
-
-
-        }, mValueA, mValueB);
+                switch (mOperation)
+                {
+                case BinaryOperators::ADDITION:
+                    return valueA->getValue() + valueB->getValue();
+                case BinaryOperators::SUBTRACTION:
+                    return valueA->getValue() - valueB->getValue();
+                case BinaryOperators::MULTIPLICATION:
+                    return valueA->getValue() * valueB->getValue();
+                case BinaryOperators::DIVISION:
+                    return valueA->getValue() / valueB->getValue();
+                }
+                throw std::runtime_error("Invalid binary operation: Invalid operation");
+            },
+            mValueA, mValueB);
 
         return value;
     }
 
-	void setValueA(ExpressionNodeContainer<T>&& value)
-	{
-		mValueA = std::move(value);
-	}
+    void setValueA(ExpressionNodeContainer<T> &&value)
+    {
+        mValueA = std::move(value);
+    }
 
-	void setValueB(ExpressionNodeContainer<T>&& value)
-	{
-		mValueB = std::move(value);
-	}
+    void setValueB(ExpressionNodeContainer<T> &&value)
+    {
+        mValueB = std::move(value);
+    }
 
-    BinaryOperators getOperation() const { return mOperation; }
-    int getOperationPrecedence() const { return operatorPrecedence.at(mOperation); }
+    BinaryOperators getOperation() const
+    {
+        return mOperation;
+    }
+    int getOperationPrecedence() const
+    {
+        return operatorPrecedence.at(mOperation);
+    }
 
     /**
      * Appends a new binary operation on mValueA or B depending on boolean appendOnLeft
@@ -130,59 +127,60 @@ public:
      * @tparam T holder type generally for the ConstantNode
      * @tparam expression New Binary operation to be added
      * @param appendOnLeft Appends on either mValueA if true or mValueB if false
-    */
+     */
     void appendBinaryOperation(std::unique_ptr<BinaryOperationNode<T>> newBinaryOperation)
     {
-        std::visit([this, newBinaryOperation](auto& child) {
-            if (newBinaryOperation->getOperationPrecedence() > child->getOperationPrecedence())
-            {
-                child->appendBinaryOperation(std::move(newBinaryOperation));
-                return;
-            }
-            
-            ExpressionNodeContainer<T> tempValue = std::move(child);
-            newBinaryOperation->setValueA(std::move(tempValue));
-            child = std::move(newBinaryOperation);
-        }, mValueB);
+        std::visit(
+            [this, newBinaryOperation](auto &child) {
+                if (newBinaryOperation->getOperationPrecedence() > child->getOperationPrecedence())
+                {
+                    child->appendBinaryOperation(std::move(newBinaryOperation));
+                    return;
+                }
+
+                ExpressionNodeContainer<T> tempValue = std::move(child);
+                newBinaryOperation->setValueA(std::move(tempValue));
+                child = std::move(newBinaryOperation);
+            },
+            mValueB);
     }
 
-protected:
+  protected:
     ExpressionNodeContainer<T> mValueA;
     ExpressionNodeContainer<T> mValueB;
-	const BinaryOperators mOperation;
+    const BinaryOperators mOperation;
 };
 
-
-template <typename T>
-class PrintNode : public StatementNode
+template <typename T> class PrintNode : public StatementNode
 {
-public:
-	PrintNode() :
-		mExpression(nullptr)
-	{}
+  public:
+    PrintNode() : mExpression(nullptr)
+    {
+    }
 
-	PrintNode(std::unique_ptr<ExpressionNode<T>> expression) :
-		mExpression(std::move(expression)) {}
+    PrintNode(std::unique_ptr<ExpressionNode<T>> expression) : mExpression(std::move(expression))
+    {
+    }
 
-	void doStatement() override
-	{
-		if (!mExpression)
-		{
-			throw std::runtime_error("Invalid print operation: Expression is nullptr");
-		}
-		std::cout << mExpression->getValue() << std::endl;
-	}
+    void doStatement() override
+    {
+        if (!mExpression)
+        {
+            throw std::runtime_error("Invalid print operation: Expression is nullptr");
+        }
+        std::cout << mExpression->getValue() << std::endl;
+    }
 
-	void setExpression(std::unique_ptr<ExpressionNode<T>> expression)
-	{
-		mExpression = std::move(expression);
-	}
+    void setExpression(std::unique_ptr<ExpressionNode<T>> expression)
+    {
+        mExpression = std::move(expression);
+    }
 
-	const ExpressionNode<T>& getExpression() const
-	{
-		return *mExpression;
-	}
-private:
-	std::unique_ptr<ExpressionNode<T>> mExpression;
+    const ExpressionNode<T> &getExpression() const
+    {
+        return *mExpression;
+    }
+
+  private:
+    std::unique_ptr<ExpressionNode<T>> mExpression;
 };
-
