6b003d44c72c1b281f7403c8e6f0bb4f016a1e58
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-12 19:45:14 +0800

    Checks if arguments too few

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "ParseTreeBuilder.hpp"
 
+#include <iostream>
+
+
 // TODO: Add argument handling
 int main(int argc, char* argv[])
 {
+    if (argc < 2)
+    {
+        std::cerr << "Arguments too few" << "\n" <<
+            "Usage: eval-ast [equation]" << std::endl;
+    }
+
     ParseTreeBuilder parseTreeBuilder("1+23.5*2");
     if (parseTreeBuilder.generateParseTree()) 
     {
