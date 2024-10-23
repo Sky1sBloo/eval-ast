c2d21cb9f46002a3a7872311f88b31bf4f892729
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-23 12:03:12 +0800

    Added parenthesis on operatorcharacters

--- src/BinaryOperators.hpp
+++ src/BinaryOperators.hpp
@@ -21,10 +21,10 @@ static const std::unordered_map<BinaryOperators, int> operatorPrecedence = {
     {BinaryOperators::ADDITION, 2}, {BinaryOperators::SUBTRACTION, 2},      {BinaryOperators::MULTIPLICATION, 3},
     {BinaryOperators::DIVISION, 3}, {BinaryOperators::OPEN_PARENTHESIS, 1}, {BinaryOperators::CLOSE_PARENTHESIS, 1}};
 
-static const std::unordered_map<char, BinaryOperators> operatorCharacters = {{'+', BinaryOperators::ADDITION},
-                                                                             {'-', BinaryOperators::SUBTRACTION},
-                                                                             {'*', BinaryOperators::MULTIPLICATION},
-                                                                             {'/', BinaryOperators::DIVISION}};
+static const std::unordered_map<char, BinaryOperators> operatorCharacters = {
+    {'+', BinaryOperators::ADDITION},         {'-', BinaryOperators::SUBTRACTION},
+    {'*', BinaryOperators::MULTIPLICATION},   {'/', BinaryOperators::DIVISION},
+    {'(', BinaryOperators::OPEN_PARENTHESIS}, {')', BinaryOperators::CLOSE_PARENTHESIS}};
 
 /**
  * Checks if the character is an operator
