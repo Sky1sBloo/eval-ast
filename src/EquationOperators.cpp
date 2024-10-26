80ae621a970f9c3ccf2259c692d5538b21b9e34a
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-26 18:27:50 +0800

    Added operatorCharacters definition in source file

--- src/EquationOperators.cpp
+++ src/EquationOperators.cpp
@@ -3,3 +3,9 @@
 const std::unordered_map<EquationOperators::Types, int> EquationOperators::precedence = {
     {Types::ADDITION, 2}, {Types::SUBTRACTION, 2},      {Types::MULTIPLICATION, 3},
     {Types::DIVISION, 3}, {Types::OPEN_PARENTHESIS, 1}, {Types::CLOSE_PARENTHESIS, 1}};
+
+const std::unordered_map<char, EquationOperators::Types> EquationOperators::operatorCharacters = {
+    {'+', Types::ADDITION},         {'-', Types::SUBTRACTION},
+    {'*', Types::MULTIPLICATION},   {'/', Types::DIVISION},
+    {'(', Types::OPEN_PARENTHESIS}, {')', Types::CLOSE_PARENTHESIS}};
+
